ef952ab206b3859ca2b007f7efdb093aec61a283
wesker-albert <github@umbrellacorp.io>
2023-08-21 02:11:56 +0000

    fix init lint

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,3 @@
-"""An experimental MUD server, written in Python. Object-oriented and event driven."""
-
 from dotenv import find_dotenv, load_dotenv
 
 load_dotenv(find_dotenv(".env.test"), override=True)
